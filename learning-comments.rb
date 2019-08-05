1f27b3e457a3d56acc4756c2370a425300cb4f31
Barrette Banner <barrettebanner@gmail.com>
2019-08-05 01:03:12 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,21 @@
+=begin
+# these are comments
+puts "Joy to the world"
+puts "All the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+name = "Byron"
+# name = "Luca"
+ 
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in
+the store."
+=end
+
+
+run_code_inside = true
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
